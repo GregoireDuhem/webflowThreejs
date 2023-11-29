159a4c496dc125f84eec32b3a614f07a6642ee5c
GregoireDuhem <gregoire.duhem@epitech.eu>
2023-11-29 15:51:35 +0100

    [+] three import for webflow

--- src/features/scrollAnim.js
+++ src/features/scrollAnim.js
@@ -1,6 +1,8 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+// import * as THREE from 'three'
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+// import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+// import { Reflector } from 'three/examples/jsm/objects/Reflector.js'
+// import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
@@ -11,27 +13,33 @@ const camera = new THREE.PerspectiveCamera(
 )
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.setClearColor(0xeeeeee, 1) // Light grey background for contrast
+renderer.setClearColor(0xffffff, 1) // Light grey background for contrast
 document.body.appendChild(renderer.domElement)
 
 // Lighting
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+const ambientLight = new THREE.AmbientLight(0xffffff, 5)
 scene.add(ambientLight)
-const pointLight = new THREE.PointLight(0xffffff, 1)
-pointLight.position.set(5, 5, 5)
+const pointLight = new THREE.PointLight(0xffffff, 200)
+pointLight.position.set(0, 2, 6)
+pointLight.distance = 100
 scene.add(pointLight)
 
-// GLTF Loader
-const loader = new GLTFLoader()
+const pointLightHelper = new THREE.PointLightHelper(pointLight)
+scene.add(pointLightHelper)
+
+// FBX Loader
+let fbxModel
+
+const loader = new FBXLoader()
 loader.load(
-  'http://127.0.0.1:5500/public/oldBag.glb',
-  (gltf) => {
-    const model = gltf.scene
-    model.scale.set(1, 1, 1) // Scale if needed
-    model.position.set(0, 0, 0) // Adjust position if needed
-    scene.add(model)
+  'https://gregoireduhem.github.io/threejsProd/newBag.fbx',
+  (fbx) => {
+    fbx.scale.set(0.1, 0.1, 0.1) // Scale if needed
+    fbx.position.set(0, -2, 0) // Adjust position if needed
+    scene.add(fbx) // Add the model directly
+    fbxModel = fbx
   },
-  undefined,
+  undefined, // Progress callback not defined
   (error) => {
     console.error('An error happened', error)
   }
@@ -41,6 +49,9 @@ camera.position.set(0, 1, 5) // Adjust camera position
 const controls = new OrbitControls(camera, renderer.domElement)
 
 function animate() {
+  if (fbxModel) {
+    fbxModel.rotation.y += 0.005
+  }
   requestAnimationFrame(animate)
   controls.update()
   renderer.render(scene, camera)
--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import animate from './features/scrollAnim'
 import './styles/style.css'
 
 console.log('Welcome to Vite + JS + Webflow!')
-
+//test
 createBadge()
 animateTitle()
 animate()
+// ;('https://cdn.jsdelivr.net/gh/GregoireDuhem/webflowThreejs/src/main.js')
+// https://cdn.jsdelivr.net/gh/GregoireDuhem/threejsProd/index.d34e7cb9.js
